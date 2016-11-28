8a9d4f7b020c94225c7418c5fdf1c80808ff2371
Francois Michonneau <francois.michonneau@gmail.com>
2016-11-28 12:08:45 -0500

    regenerate lessons

--- handout-script.R
+++ handout-script.R
@@ -74,7 +74,7 @@ example_data <- data.frame(animal=c("dog", "cat", "sea cucumber", "sea urchin"),
                            feel=c("furry", "furry", "squishy", "spiny"),
                            weight=c(45, 8, 1.1, 0.8), stringsAsFactors=FALSE)
 str(example_data)
-## ## Challenge
+## ## Challenge:
 ## ##  There are a few mistakes in this hand crafted `data.frame`,
 ## ##  can you spot and fix them? Don't hesitate to experiment!
 ## author_book <- data.frame(author_first=c("Charles", "Ernst", "Theodosius"),
@@ -94,9 +94,19 @@ str(example_data)
 ##                                temperature=c(10, 30, 18, "15"),
 ##                                northern_hemisphere=c(TRUE, TRUE, FALSE, "FALSE"),
 ##                                has_kangaroo=c(FALSE, FALSE, FALSE, 1))
+##  ## Challenge:
+##  ##   We introduced you to the `data.frame()` function and `read.csv()`,
+##  ##   but what if we are starting with some vectors? The best way to do
+##  ##   this is to pass those vectors to the `data.frame()` function, similar
+##  ##   to the above.
+##  color <- c("red", "green", "blue", "yellow")
+##  counts <- c(50, 60, 65, 82)
+##  new_datarame <- data.frame(colors = color, counts = counts)
+##  ##    Try making your own new data frame from some vectors. You can check
+##  ##    the data type of the new object using `class()`.
 
 ## Sequences and Subsetting data frames
-
+### Challenges:
 ### 1. The function `nrow()` on a `data.frame` returns the number of
 ### rows. Use it, in conjuction with `seq()` to create a new
 ### `data.frame` called `surveys_by_10` that includes every 10th row
@@ -114,6 +124,85 @@ str(example_data)
 ###   excluding the 7th through final row of the `surveys` dataset.
 
 
+## The dplyr lesson
+## ## Pipes Challenge:
+## ##  Using pipes, subset the data to include individuals collected
+## ##  before 1995, and retain the columns `year`, `sex`, and `weight.`
+## 
+## ## Mutate Challenge:
+## ##  Create a new dataframe from the survey data that meets the following
+## ##  criteria: contains only the `species_id` column and a column that
+## ##  contains values that are half the `hindfoot_length` values (e.g. a
+## ##  new column `hindfoot_half`). In this `hindfoot_half` column, there are
+## ##  no NA values and all values are < 30.
+## 
+## ##  Hint: think about how the commands should be ordered to produce this data frame!
+## 
+## ## Tally Challenges:
+## ##  1. How many individuals were caught in each `plot_type` surveyed?
+## 
+## ##  2. Use `group_by()` and `summarize()` to find the mean, min, and
+## ##  max hindfoot length for each species (using `species_id`).
+## 
+## ##  3. What was the heaviest animal measured in each year? Return the
+## ##  columns `year`, `genus`, `species_id`, and `weight`.
+## 
+## ## 4. You saw above how to count the number of individuals of each `sex` using a
+## ## combination of `group_by()` and `tally()`. How could you get the same result
+## ## using `group_by()` and `summarize()`? Hint: see `?n`.
+## 
+## ### Create the dataset for exporting:
+## ##  Start by removing observations for which the `species_id`, `weight`,
+## ##  `hindfoot_length`, or `sex` data are missing:
+## surveys_complete <- surveys %>%
+##   filter(species_id != "",        # remove missing species_id
+##   !is.na(weight),                 # remove missing weight
+## 		  !is.na(hindfoot_length),        # remove missing hindfoot_length
+## 		  sex != "")                      # remove missing sex
+## 
+## ##  Now remove rare species in two steps. First, make a list of species which
+## ##  appear at least 50 times in our dataset:
+## species_counts <- surveys_complete %>%
+##               group_by(species_id) %>%
+##               tally %>%
+## 				          filter(n >= 50) %>%
+## 				          select(species_id)
+## 
+## ##  Second, keep only those species:
+## surveys_complete <- surveys_complete %>%
+##              filter(species_id %in% species_counts$species_id)
+## 
 
+
+## The ggplot2 lesson
+## ## Challenges:
+## ##  Start with the boxplot we created:
+## ggplot(data = surveys_complete, aes(x = species_id, y = hindfoot_length)) +
+##   geom_boxplot(alpha = 0) +
+##   geom_jitter(alpha = 0.3, color = "tomato")
+## 
+## ##  1. Replace the box plot with a violin plot; see `geom_violin()`.
+## 
+## ##  2. Represent weight on the log10 scale; see `scale_y_log10()`.
+## 
+## ##  3. Create boxplot for `hindfoot_length`.
+## 
+## ##  4. Add color to the datapoints on your boxplot according to the
+## ##  plot from which the sample was taken (`plot_id`).
+## ##  Hint: Check the class for `plot_id`. Consider changing the class
+## ##  of `plot_id` from integer to factor. Why does this change how R
+## ##  makes the graph?
+## 
+## ## Plotting time series challenge:
+## ##  Use what you just learned to create a plot that depicts how the
+## ##  average weight of each species changes through the years.
+## 
+## ## Final plotting challenge:
+## ##  With all of this information in hand, please take another five
+## ##  minutes to either improve one of the plots generated in this
+## ##  exercise or create a beautiful graph of your own. Use the RStudio
+## ##  ggplot2 cheat sheet for inspiration:
+## ##  https://www.rstudio.com/wp-content/uploads/2015/08/ggplot2-cheatsheet.pdf
+## 
 
 
