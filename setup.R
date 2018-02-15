528dab9369cc1ac102aa71ac2b745fdc46d019cf
François Michonneau <francois.michonneau@gmail.com>
2018-02-15 12:25:49 -0500

    create knitr engine for text based answers

--- setup.R
+++ setup.R
@@ -45,3 +45,16 @@ knitr::knit_hooks$set(answer = function(before, options, envir) {
         paste("</div>", "</div>", sep = "\n")
     }
 })
+
+eng_text_answer <- knitr:::eng_html_asset(
+                               paste(
+                                   "<div class=\"accordion\">",
+                                   "<h3 class=\"toc-ignore\">Answer</h3>",
+                                   "<div style=\"background: #fff;\">",
+                                   "<p>",  sep = "\n"),
+                               paste(
+                                   "</div>", "</div>", "</p>", sep = "\n"
+                               )
+                           )
+
+knitr::knit_engines$set(text_answer = eng_text_answer)
