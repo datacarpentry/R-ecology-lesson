e4079b4c99d0f8039941a8c7a5ddee71aa7dda5a
François Michonneau <francois.michonneau@gmail.com>
2019-03-27 12:25:56 +0100

    use checker for link validation

--- build_lesson.R
+++ build_lesson.R
@@ -14,32 +14,21 @@ build_lesson <- function() {
 }
 
 
-format_url_errors <- function(res_links, idx) {
-    paste0("  - broken url:", res_links$urlname[idx], "\n",
-           "          from:", res_links$parentname[idx], "\n")
-}
-
 CheckLinks <- R6::R6Class(
      "CheckLinks", inherit = TicStep,
      public = list(
-         run = function() {
-         ## ignore JS file not included as part of rmarkdown
-         ## ignore email addresses
-         ## ignore embedded images
-         link_status <- system("linkchecker --ignore-url=external.+js --ignore-url=^mailto: --ignore-url=^data: --no-warnings  --file-output=csv/link_res.csv _site")
-         message("linkchecker exit code: ", link_status)
-         ## write output to CSV file and check error codes
-         ## stop only for 404s
-         res_links <- readr::read_delim("link_res.csv", delim = ";", comment = "#")
-         unique(res_links$result)
-         res_404 <- grepl("^404|^gaierror", res_links$result)
-         other_errors <- grepl("error", res_links$result, ignore.case = TRUE)
-         if (any(other_errors)) warning("These links might be problematic: \n",
-                                        format_url_errors(res_links, other_errors))
-         if (any(res_404)) stop("Some links are broken (check log to inspect): \n",
-                                format_url_errors(res_links, res_404))
-     })
-     )
+       run = function() {
+         res <- checker::check_links("_site",
+           show_summary = TRUE,
+           check_external = TRUE,
+           only_with_issues = FALSE,
+           ignore_pattern = "site_libs"
+         )
+         broken_links <- res[res$tag_type == "a" & (!res$valid & !is.na(res$valid)), ]
+         if (nrow(broken_links) > 0)
+           warning("Some links are broken, see summary above.")
+       })
+)
 
 check_links <- function() {
     CheckLinks$new()
