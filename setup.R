0cc4baf50cb429a563f149d8df4aea3465ab5bd1
Katrin Leinweber <Katrin.Leinweber@tib.eu>
2019-09-05 17:48:52 +0200

    Fix typos

--- setup.R
+++ setup.R
@@ -1,5 +1,5 @@
 ## file structure
-if (! file.exists("data_raw")) dir.create("data_raw")
+if (!file.exists("data_raw")) dir.create("data_raw")
 
 if (!file.exists("data_raw/surveys.csv")) {
     download.file("https://ndownloader.figshare.com/files/2292172",
