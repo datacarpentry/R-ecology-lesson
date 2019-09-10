7cee8f341eb2177d7bd4acd10a75c0ee5000f038
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2019-09-10 11:23:03 +0200

    change data and raw data paths (#563)
    * Put raw data in folder with longer name to discourage intervention in it
    
    * Shorten figure folder name to harmonise with d4dd81a
    
    * Align working-dir screenshot with file- & folder-related instructions
    
    * Fix typos

--- setup.R
+++ setup.R
@@ -1,26 +1,26 @@
 ## file structure
-if (! file.exists("data")) dir.create("data")
+if (! file.exists("data_raw")) dir.create("data_raw")
 
-if (!file.exists("data/surveys.csv")) {
+if (!file.exists("data_raw/surveys.csv")) {
     download.file("https://ndownloader.figshare.com/files/2292172",
-                  "data/surveys.csv")
+                  "data_raw/surveys.csv")
 }
-if (!file.exists("data/species.csv")) {
+if (!file.exists("data_raw/species.csv")) {
     download.file("https://ndownloader.figshare.com/files/3299483",
-                  "data/species.csv")
+                  "data_raw/species.csv")
 }
-if (!file.exists("data/plots.csv")) {
+if (!file.exists("data_raw/plots.csv")) {
     download.file("https://ndownloader.figshare.com/files/3299474",
-                  "data/plots.csv")
+                  "data_raw/plots.csv")
 }
-if (!file.exists("data/portal_data_joined.csv")) {
+if (!file.exists("data_raw/portal_data_joined.csv")) {
     download.file("https://ndownloader.figshare.com/files/2292169",
-                  "data/portal_data_joined.csv")
+                  "data_raw/portal_data_joined.csv")
 }
 
-if (!file.exists("data/portal_mammals.sqlite")) {
+if (!file.exists("data_raw/portal_mammals.sqlite")) {
     download.file("https://ndownloader.figshare.com/files/2292171",
-                  "data/portal_mammals.sqlite")
+                  "data_raw/portal_mammals.sqlite")
 }
 
 
