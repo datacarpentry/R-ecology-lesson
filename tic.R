66e86faeab8ba08aabd9f449f92bd73c05ebece8
François Michonneau <francois.michonneau@gmail.com>
2020-06-24 11:28:18 +0200

    modernize CI

--- tic.R
+++ tic.R
@@ -1,14 +1,17 @@
 source("build_lesson.R")
 
 get_stage("before_install") %>%
+  add_code_step(install.packages("git2r")) %>%
   add_code_step(update.packages(ask = FALSE))
 
 get_stage("install") %>%
-  add_code_step(remotes::install_deps(dependencies = TRUE))
+  add_code_step(system("python -m pip install update-copyright")) %>%
+  add_code_step(remotes::install_deps(dependencies = TRUE)) %>%
+  add_step(step_install_github("fmichonneau/checker"))
 
 get_stage("deploy") %>%
-    add_step(build_lesson()) %>%
-    add_step(check_links())
+  add_step(build_lesson()) %>%
+  add_step(check_links())
 
 if (Sys.getenv("id_rsa") != "") {
   # pkgdown documentation can be built optionally. Other example criteria:
@@ -17,19 +20,19 @@ if (Sys.getenv("id_rsa") != "") {
   # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
   # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
   get_stage("before_deploy") %>%
-      add_step(step_setup_ssh())
+    add_step(step_setup_ssh())
 
-    ## if there is a tag associated with the push or we are in master, the
-    ## lesson gets deployed on gh-pages, and rendered by GitHub
-    if (ci()$get_branch() == "master" || ci()$is_tag()) {
-        get_stage("deploy") %>%
-            add_step(step_push_deploy(path = "_site", branch = "gh-pages"))
-    }
+  ## if there is a tag associated with the push or we are in main, the
+  ## lesson gets deployed on gh-pages, and rendered by GitHub
+  if (ci()$get_branch() == "main" || ci()$is_tag()) {
+    get_stage("deploy") %>%
+      add_step(step_push_deploy(path = "_site", branch = "gh-pages"))
+  }
 
-    ## if the branch is `tidyverse-first` the lesson gets deployed to the
-    ## development branch, and will be rendered by netlify
-    if (ci()$get_branch() == "tidyverse-first") {
-        get_stage("deploy") %>%
-            add_step(step_push_deploy(path = "_site", branch = "development"))
-    }
+  ## if the branch is `tidyverse-first` the lesson gets deployed to the
+  ## development branch, and will be rendered by netlify
+  if (ci()$get_branch() == "tidyverse-first") {
+    get_stage("deploy") %>%
+      add_step(step_push_deploy(path = "_site", branch = "development"))
+  }
 }
