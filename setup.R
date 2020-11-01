8e740ec8c693df0364c11c07227645bd7971329b
Tobias Busch <teebusch@gmail.com>
2020-11-01 15:58:14 +0100

    Use custom blocks (:::) for challenges
    This makes it easier to edit them, because syntax highlighting works  and no blockquote characters are needed.
    It allows to style them separately, and is semenatically cleaner (challenges are NOT quotes).
    Custom blocks are the preferred way to do custom styling of divs in RMarkdown

--- setup.R
+++ setup.R
@@ -36,25 +36,29 @@ knitr::opts_chunk$set(results='hide', fig.path='img/R-ecology-',
 ## hook for challenges answers
 
 knitr::knit_hooks$set(answer = function(before, options, envir) {
-    if (before) {
-        paste(
-            "<div class=\"accordion\">",
-              "<h3 class=\"toc-ignore\">Answer</h3>",
-              "<div style=\"background: #fff;\">",  sep = "\n")
-    } else {
-        paste("</div>", "</div>", sep = "\n")
-    }
+  if (before) {
+    paste(
+      "<div class=\"accordion\">",
+      "<h3 class=\"toc-ignore\">Answer</h3>",
+      "<div class=\"answer\">",
+      sep = "\n"
+    )
+  } else {
+    paste("</div>", "</div>", sep = "\n")
+  }
 })
 
 eng_text_answer <- knitr:::eng_html_asset(
-                               paste(
-                                   "<div class=\"accordion\">",
-                                   "<h3 class=\"toc-ignore\">Answer</h3>",
-                                   "<div style=\"background: #fff;\">",
-                                   "<p>",  sep = "\n"),
-                               paste(
-                                   "</div>", "</div>", "</p>", sep = "\n"
-                               )
-                           )
+  paste(
+    "<div class=\"accordion\">",
+    "<h3 class=\"toc-ignore\">Answer</h3>",
+    "<div class=\"answer\">",
+    "<p>",
+    sep = "\n"
+  ),
+  paste(
+    "</p>", "</div>", "</div>", sep = "\n"
+  )
+)
 
 knitr::knit_engines$set(text_answer = eng_text_answer)
